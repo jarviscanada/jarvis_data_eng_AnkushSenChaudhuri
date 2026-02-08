6ace82aab7593dc35d7de4546e1f7e08410f8cda
AnkushSC31 <ankush.schaudhuri@gmail.com>
2026-02-08 10:29:34 -0500

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -21,30 +21,6 @@ cpu_architecture=$(echo "$lscpu_out" | grep "^Architecture:" | awk '{print $2}' 
 cpu_model=$(echo "$lscpu_out" | sed -n 's/^Model name:[[:space:]]*//p' | xargs)
 
 cpu_mhz=$(awk -F': ' '/^cpu MHz/{print $2; exit}' /proc/cpuinfo | xargs)
-                                                                                 1,1           Top
-#!/bin/bash
-
-# validate args
-if [ "$#" -ne 5 ]; then
-  echo "Illegal number of parameters"
-  exit 1
-fi
-
-psql_host=$1
-psql_port=$2
-db_name=$3
-psql_user=$4
-psql_password=$5
-
-hostname=$(hostname -f)
-
-lscpu_out=$(lscpu)
-
-cpu_number=$(echo "$lscpu_out" | grep "^CPU(s):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out" | grep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out" | sed -n 's/^Model name:[[:space:]]*//p' | xargs)
-
-cpu_mhz=$(awk -F': ' '/^cpu MHz/{print $2; exit}' /proc/cpuinfo | xargs)
 
 l2_cache=$(echo "$lscpu_out" | awk -F: '/^L2 cache:/{print $2}' | grep -oE '[0-9]+' | head -1)
 
